fe93472cab03db3a7056dc23628a64b5302e8649
syket <syketweb@gmail.com>
2021-07-24 13:41:58 +0600

    Exercise 1 done

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -2,20 +2,23 @@
 // http://localhost:3000/isolated/exercise/01.js
 
 import * as React from 'react'
+import {setSourceMapRange} from 'typescript'
+
+const {useState} = React
 
 function Greeting() {
   // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+  const [name, setName] = useState('Syket')
 
   function handleChange(event) {
-    // 🐨 update the name here based on event.target.value
+    setName(event.target.value)
   }
 
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input value={name} onChange={handleChange} id="name" />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
